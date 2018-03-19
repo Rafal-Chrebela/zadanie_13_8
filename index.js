17f1af4d86599b09dfc8b3e108824dbb9c13fce9
Rafał Chrebela <rafal.chrebela@gmail.com>
2018-03-20 00:22:57 +0100

    Add files with content

---
+++ index.js
@@ -0,0 +1,22 @@
+var http = require("http");
+var fs = require("fs");
+var server = http.createServer();
+
+server.on("request", function(request, response) {
+  response.setHeader("Content-Type", "text/html");
+  if (request.method === "GET" && request.url === "/") {
+    fs.readFile("./index.html", function(err, data) {
+      response.write(data);
+      response.end();
+    });
+  } else {
+    response.setHeader("Content-Type", "image/jpg");
+    response.statusCode = 404;
+    fs.readFile("./cat.jpg", function(err, data) {
+      response.write(data);
+      response.end();
+    });
+  }
+});
+
+server.listen(8080);
